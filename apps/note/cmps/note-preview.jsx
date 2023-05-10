e9a8d88238ec630595eaae10d27aef17f2df1b97
Omer Hassin <omerhassin21@gmail.com>
2023-05-10 12:52:08 +0300

    "Added LongTXT.jsx file"

---
+++ apps/note/cmps/note-preview.jsx
@@ -0,0 +1,7 @@
+export function NotePreview(){
+    return (
+        <article className="note-preview">
+            <h2>Note Text</h2>
+        </article>
+    )
+}
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,45 @@
-// note service
+import { storageService } from "../../../services/async-storage.service.js"
+import { utilService } from "../../../services/util.service.js"
+
+const NOTE_KEY = 'noteDB'
+_createNotes()
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyNote,
+}
+
+function query() {
+    return storageService.query(NOTE_KEY)
+        .then(notes => notes)
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+    // return axios.get(NOTE_KEY, noteId)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+        return storageService.put(NOTE_KEY, note)
+    } else {
+        return storageService.post(NOTE_KEY, note)
+    }
+}
+
+function getEmptyNote(type) {
+    return {
+        id: '',
+        type,
+        title: 'Title',
+        txt: 'text',
+        label: "Important",
+    }
+}
