0dca83d262819acd13f7f73123dfca466c3485c9
Omer Hassin <omerhassin21@gmail.com>
2023-05-12 14:28:10 +0300

    added image and video display

--- apps/note/cmps/dynamic-inputs/note-img.jsx
+++ apps/note/cmps/dynamic-inputs/note-img.jsx
@@ -1,21 +1,28 @@
-import { LongTxt } from "../../../../cmps/long-txt.jsx"
 import { noteService } from "../../services/note.service.js"
 
 const {useRef} = React
 
 export function NoteImg({note}) {
     const noteTitleRef = useRef(null)
+	const noteTxtRef = useRef(null)
 
     function changeContentTitle(ev) {
         note.info.title = noteTitleRef.current.innerText
         noteService.save(note)
     }
 
+    function changeContent(ev) {
+        note.info.txt = noteTxtRef.current.innerText
+        noteService.save(note)
+    }
+
     return <section className="note-img" >
-        <h3 ref={noteTitleRef} onKeyUp={(ev) => changeContentTitle(ev)} contentEditable={true} suppressContentEditableWarning={true}>{note.info.title}</h3>
-        <LongTxt txt={note.info.txt} length={100} />
-        <img src={'https://www.google.com/search?q=fpl+fantasy&sxsrf=APwXEdcFQjMEiHhkg4jensPrfwDFDIt5Ew:1683827582237&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiBw5vH6u3-AhWCdKQEHQXpBLgQ_AUoAnoECAEQBA&biw=1003&bih=919&dpr=1#imgrc=N4YuYRg0ewt72M'} alt={note.info.title} />
-        {/* <img src={note.info.url} alt={note.info.title} /> */}
-        <p>{note.type}</p>
+        <React.Fragment>
+            <section className="note-img">
+                <h3 ref={noteTitleRef} onKeyUp={(ev) => changeContentTitle(ev)} contentEditable={true} suppressContentEditableWarning={true}>{note.info.title}</h3>
+                <img src={note.info.url} />
+                <p ref={noteTxtRef} onKeyUp={(ev) => changeContent(ev)} contentEditable={true} suppressContentEditableWarning={true}>{note.info.txt}</p>
+            </section>
+        </React.Fragment>
     </section>
 }
