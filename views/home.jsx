f71d0c9d26ea3b95e7bea23d5e3b2da3a347577f
Omer Hassin <omerhassin21@gmail.com>
2023-05-13 18:47:06 +0300

    made change to links in home

--- views/home.jsx
+++ views/home.jsx
@@ -7,27 +7,27 @@ export function Home() {
       <p>Appsus is a versatile application that brings together various productivity tools in one place.</p>
 
       <div className="features">
-        <div className="feature">
-          <Link to="/note">
-            <img src="./assets/img/keep.svg" alt="Google Keep Clone" className="feature-image" />
-          </Link>
-          <h2>Google Keep Clone</h2>
-          <p>
-            Stay organized with our Google Keep clone, a note-taking app that allows you to create, organize, and manage your notes
-            seamlessly.
-          </p>
-        </div>
+        <Link to="/note">
+          <div className="feature">
+              <img src="./assets/img/keep.svg" alt="Google Keep Clone" className="feature-image" />
+            <h2>Google Keep Clone</h2>
+            <p>
+              Stay organized with our Google Keep clone, a note-taking app that allows you to create, organize, and manage your notes
+              seamlessly.
+            </p>
+          </div>
+        </Link>
 
-        <div className="feature">
-          <Link to="/mail">
+        <Link to="/mail">
+          <div className="feature">
             <img src="./assets/img/gmail.svg" alt="Gmail Clone" className="feature-image" />
-          </Link>
-          <h2>Gmail Clone</h2>
-          <p>
-            Manage your emails efficiently with our Gmail clone, providing a familiar and intuitive interface for effective communication.
-          </p>
-        </div>
-      </div>
+            <h2>Gmail Clone</h2>
+            <p>
+              Manage your emails efficiently with our Gmail clone, providing a familiar and intuitive interface for effective communication.
+            </p>
+          </div>
+        </Link>
+          </div>
     </section>
   );
 }
